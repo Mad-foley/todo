d19b17f2bce2598c774464b2c9d118a415050b4b
Maddy Foley <maddyfoley5@gmail.com>
2023-06-22 12:09:47 -0400

    added get method for items

--- server/src/routes/item.js
+++ server/src/routes/item.js
@@ -4,17 +4,26 @@ import { ItemModel } from '../models/item.js';
 
 const router = express.Router();
 
-router.post("/:id", async(req, res) => {
+//get all items in category
+router.get("/:category_id", async(req, res) => {
+    try{
+        const items = await ItemModel.find({itemCategory: req.params.category_id})
+        return res.json(items)
+    } catch (err) {
+        return res.json(err);
+    }
+})
+
+router.post("/:category_id", async(req, res) => {
     const {name, startDate, endDate, isCompleted } = req.body;
     try{
-        const category = await CategoryModel.findById({_id: req.params.id})
+        const category = await CategoryModel.findById({_id: req.params.category_id})
         const item = new ItemModel({name, startDate, endDate, isCompleted, itemCategory: category._id});
         const response = await item.save();
         return res.json(response)
     } catch (err){
         return res.json(err)
     }
-
 });
 
 export { router as itemRouter }
